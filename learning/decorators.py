a869a8b44266707490ec89d1c647949d83a2a9f0
Ultra-Code <mega.alpha100@gmail.com>
2023-07-27 08:09:19 +0000

    learn: continue work on decorators
    Add error already produced by ruff

--- learning/decorators.py
+++ learning/decorators.py
@@ -1,13 +1,35 @@
-import typing
+import logging
+from collections.abc import Callable
+from typing import ParamSpec, TypeVar
+
+T = TypeVar("T")
+P = ParamSpec("P")
 
 
-def my_decorator(func: typing.Callable[...]) -> typing.Callable:
-    def wrapper() -> None:
+# https://docs.python.org/3/library/typing.html?highlight=callable#typing.Callable
+def my_decorator(func: Callable[P, T]) -> Callable[P, None]:
+    def wrapper(*args: P.args, **kwargs: P.kwargs) -> None:
         print("Something is happening before the function is called.")
-        func()
+        func(*args, **kwargs)
         print("Something is happening after the function is called.")
 
     return wrapper
+
+
+def add_logging(func: Callable[P, T]) -> Callable[P, T]:
+    """A type-safe decorator to add logging to a function."""
+
+    def inner(*args: P.args, **kwargs: P.kwargs) -> T:
+        logging.info(f"{func.__name__} was called")
+        return func(*args, **kwargs)
+
+    return inner
+
+
+@add_logging
+def add_two(x: float, y: float) -> float:
+    """Add two numbers together."""
+    return x + y
 
 
 def say_whee() -> None:
